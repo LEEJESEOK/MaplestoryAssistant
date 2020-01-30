68b3127d9b9e47026d60177f2395119f8a58b47a
Junelle <junelle@koreatech.ac.kr>
2020-01-30 22:29:11 +0900

    AlarmActivity Sample

--- app/src/main/java/com/android/maplestoryassistant/MainActivity.java
+++ app/src/main/java/com/android/maplestoryassistant/MainActivity.java
@@ -1,14 +1,20 @@
 package com.android.maplestoryassistant;
 
+import android.os.Bundle;
+import android.widget.TimePicker;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    TimePicker timePicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.alarmTimePicker);
+        timePicker.setIs24HourView(true);
     }
 }
