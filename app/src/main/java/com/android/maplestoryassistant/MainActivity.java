1660b168fe7cff24a627d7ce305a088cc806b5ae
LEEJESEOK <junelle@koreatech.ac.kr>
2020-02-02 17:22:19 +0900

    Add ID of resource Add input form of other map

--- app/src/main/java/com/android/maplestoryassistant/MainActivity.java
+++ app/src/main/java/com/android/maplestoryassistant/MainActivity.java
@@ -1,20 +1,53 @@
 package com.android.maplestoryassistant;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TimePicker timePicker;
+    TimePicker alarmTimePicker;
+    Spinner channelSpinner;
 
+    RadioGroup mapRadioGroup;
+    RadioButton otherMapRadioButton;
+
+    LinearLayout otherMapLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timePicker = findViewById(R.id.alarmTimePicker);
-        timePicker.setIs24HourView(true);
+        alarmTimePicker = findViewById(R.id.AlarmTimePicker);
+        alarmTimePicker.setIs24HourView(true);
+
+        channelSpinner = findViewById(R.id.ChannelSpinner);
+        ArrayAdapter channelAdapter = ArrayAdapter.createFromResource(this, R.array.channel, android.R.layout.simple_spinner_item);
+        channelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        channelSpinner.setAdapter(channelAdapter);
+
+        mapRadioGroup = findViewById(R.id.MapRadioGroup);
+        otherMapRadioButton = findViewById(R.id.OtherMapRadioButton);
+        otherMapLayout = findViewById(R.id.OtherMapLayout);
+
+        mapRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.OtherMapRadioButton) {
+                    Toast.makeText(MainActivity.this, "직접입력 선택", Toast.LENGTH_LONG).show();
+                    otherMapLayout.setVisibility(View.VISIBLE);
+                } else {
+                    otherMapLayout.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
